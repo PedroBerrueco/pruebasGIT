5d873634bc94c8653fdcbb66513c7d27e2eea664
pedroberrueco <pedroberrueco@hotmail.com>
2023-06-11 10:26:43 +0200

    commit root

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author paberrueco
+ *
+ */
+module PruebaGIT {
+}
---
+++ src/pruebagit/PruebaGIT.java
@@ -0,0 +1,14 @@
+package pruebagit;
+
+public class PruebaGIT {
+
+	public static void main(String[] args) {
+		
+		String nombre = "Pedro";
+		
+		
+		System.out.println("Hola soy " + nombre );
+
+	}
+
+}
